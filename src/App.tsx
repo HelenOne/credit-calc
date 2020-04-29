dc78dbcda9012a1235a29b437fe60d55997231d8
HelenOne <h.bright@yandex.ru>
2020-04-29 17:37:11 +0300

    fixed review issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import { Calculator } from "./Calculator";
-// import catImage from "../assets/cat.svg";
 
 export const App = () => {
   return (
     <div>
       <Calculator />
-      {/* <img className="image-cat" src={catImage} alt="catImage" /> */}
     </div>
   );
 };
--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -2,50 +2,62 @@ import * as React from "react";
 
 export const Calculator = () => {
   const [creditSum, setCreditSum] = React.useState("");
-  const [creditProcent, setCreditProcent] = React.useState("");
+  const [creditpercent, setCreditPercent] = React.useState("");
   const [creditPeriod, setCreditPeriod] = React.useState("");
   const [mounthlyPayment, setMounthlyPayment] = React.useState("");
 
   const [annuityRate, setAnnuityRate] = React.useState(0);
 
-  const handleChange = React.useCallback(
-    (
-      inputName:
-        | "creditSum"
-        | "creditProcent"
-        | "creditPeriod"
-        | "mounthlyPayment"
-    ) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (inputName === "creditSum") {
-        setCreditSum(event.target.value);
-        setMounthlyPayment(
-          (annuityRate * parseInt(event.target.value, 10)).toFixed(2)
-        );
-      } else if (inputName === "creditProcent") {
-        setCreditProcent(event.target.value);
-        const procent = parseInt(event.target.value, 10);
-        const period = parseInt(creditPeriod, 10);
-        const annuityRateValue =
-          ((procent / 100) * (1 + procent / 100) ** period) /
-          ((1 + procent / 100) ** period - 1);
-        setAnnuityRate(annuityRateValue);
-        setMounthlyPayment(
-          (annuityRateValue * parseInt(creditSum, 10)).toFixed(2)
-        );
-      } else if (inputName === "creditPeriod") {
-        setCreditPeriod(event.target.value);
-        const procent = parseInt(creditProcent, 10);
-        const period = parseInt(event.target.value, 10);
-        const annuityRateValue =
-          ((procent / 100) * (1 + procent / 100) ** period) /
-          ((1 + procent / 100) ** period - 1);
-        setAnnuityRate(annuityRateValue);
-        setMounthlyPayment(
-          (annuityRateValue * parseInt(creditSum, 10)).toFixed(2)
-        );
-      }
+  const handleCreditSumChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCreditSum(event.target.value);
+      setMounthlyPayment(
+        !Number.isNaN(annuityRate)
+          ? (annuityRate * parseInt(event.target.value, 10)).toFixed(2)
+          : ""
+      );
     },
-    [annuityRate, creditPeriod, creditProcent, creditSum]
+    [annuityRate]
+  );
+
+  const handleCreditPercent = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCreditPercent(event.target.value);
+      const percent = parseInt(event.target.value, 10);
+      const period = parseInt(creditPeriod, 10);
+      const annuityRateValue =
+        ((percent / 100) * (1 + percent / 100) ** period) /
+        ((1 + percent / 100) ** period - 1);
+      setAnnuityRate(annuityRateValue);
+      const mounthlyPaymentValue = annuityRateValue * parseInt(creditSum, 10);
+
+      setMounthlyPayment(
+        !Number.isNaN(mounthlyPaymentValue)
+          ? mounthlyPaymentValue.toFixed(2)
+          : ""
+      );
+    },
+    [creditPeriod, creditSum]
+  );
+
+  const handleCreditPeriod = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCreditPeriod(event.target.value);
+      const percent = parseInt(creditpercent, 10);
+      const period = parseInt(event.target.value, 10);
+      const annuityRateValue =
+        ((percent / 100) * (1 + percent / 100) ** period) /
+        ((1 + percent / 100) ** period - 1);
+      setAnnuityRate(annuityRateValue);
+      const mounthlyPaymentValue = annuityRateValue * parseInt(creditSum, 10);
+
+      setMounthlyPayment(
+        !Number.isNaN(mounthlyPaymentValue)
+          ? mounthlyPaymentValue.toFixed(2)
+          : ""
+      );
+    },
+    [creditSum, creditpercent]
   );
 
   return (
@@ -59,17 +71,17 @@ export const Calculator = () => {
             id="credit-sum"
             value={creditSum}
             type="number"
-            onChange={handleChange("creditSum")}
+            onChange={handleCreditSumChange}
           />
         </label>
-        <label className="input-label" htmlFor="credit-procent">
-          <span>Credit Procent (per month) </span>
+        <label className="input-label" htmlFor="credit-percent">
+          <span>Credit Percent (per month) </span>
           <input
-            className="credit-procent"
-            id="credit-procent"
-            value={creditProcent}
+            className="credit-percent"
+            id="credit-percent"
+            value={creditpercent}
             type="number"
-            onChange={handleChange("creditProcent")}
+            onChange={handleCreditPercent}
           />
         </label>
         <label className="input-label" htmlFor="credit-period">
@@ -79,19 +91,13 @@ export const Calculator = () => {
             id="credit-period"
             value={creditPeriod}
             type="number"
-            onChange={handleChange("creditPeriod")}
+            onChange={handleCreditPeriod}
           />
         </label>
-        <label className="input-label" htmlFor="mounthly-payment">
+        <div className="mounthly-payment">
           <span>Mounthly Payment</span>
-          <input
-            className="mounthly-payment"
-            id="mounthly-payment"
-            value={mounthlyPayment}
-            type="number"
-            onChange={() => {}}
-          />
-        </label>
+          <span>{mounthlyPayment || "0.00"}</span>
+        </div>
       </div>
     </div>
   );
